88d0e03cb842b012ef62dff7e7cb38bdaab62d06
Sid <sid@Sids-MacBook-Pro.local>
2019-12-07 15:06:46 -0500

    big brain time. maybe.

--- src/encoders/rnn_seq_encoder.py
+++ src/encoders/rnn_seq_encoder.py
@@ -173,7 +173,7 @@ class RNNEncoder(SeqEncoder):
                 # self.attention = BahdanauAttention(self.batch_seq_len)
                 # Do attention on each timestep
                 print("Batch Seq Len: ", self.batch_seq_len)
-                print("Token Embeddings: ", self.token_embeddings.get_shape())
+                print("Token Embeddings: ", self.get_hyper("code_batch_size"))
                 batch_num = self.token_embeddings.shape.dims[1].value
                 self.weights = tf.zeros([batch_num, 1, self.batch_seq_len])
                 self.ctx_v = tf.zeros(tf.shape(x[:, 0:1, :]))
