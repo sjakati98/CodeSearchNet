7a3997b6dac76a5085eca6424b8cacf287d113c2
Sid <sid@Sids-MacBook-Pro.local>
2019-12-07 22:09:49 -0500

    hope

--- src/encoders/rnn_seq_encoder.py
+++ src/encoders/rnn_seq_encoder.py
@@ -235,7 +235,7 @@ class RNNEncoder(SeqEncoder):
 
         # Concat Stuff
         new_ctx = tf.transpose(new_ctx, perm=[0, 2, 1])
-        self.ctx_v = tf.concat((self.ctx_v, new_ctx), 1)
+        self.ctx_v = tf.concat((self.ctx_v, new_ctx), 0)
 
         return new_ctx
 
