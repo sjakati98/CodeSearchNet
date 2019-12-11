59aa30c5cca22f8fdf191ddb8fdfb7a810f3b88d
Mukul Kudlugi <mukul.kudlugi@gmail.com>
2019-12-11 15:58:18 -0500

    Added cbow encoder

--- src/encoders/cbow_seq_encoder.py
+++ src/encoders/cbow_seq_encoder.py
@@ -6,7 +6,7 @@ from .masked_seq_encoder import MaskedSeqEncoder
 from utils.tfutils import pool_sequence_embedding
 
 
-class NBoWEncoder(MaskedSeqEncoder):
+class CBoWEncoder(MaskedSeqEncoder):
     @classmethod
     def get_default_hyperparameters(cls) -> Dict[str, Any]:
         encoder_hypers = { 'nbow_pool_mode': 'weighted_mean',
@@ -22,30 +22,11 @@ class NBoWEncoder(MaskedSeqEncoder):
         with tf.variable_scope("nbow_encoder"):
             self._make_placeholders()
 
-            self.seq_tokens_embeddings = self.embedding_layer(self.placeholders['tokens']) # batch size x max seq len x emb dim
+            seq_tokens_embeddings = self.embedding_layer(self.placeholders['tokens'])
             seq_token_mask = self.placeholders['tokens_mask']
             seq_token_lengths = tf.reduce_sum(seq_token_mask, axis=1)  # B
-
-            batch_seq_len = self.seq_tokens_embeddings.get_shape().dims[1].value
-
-            # pad seqs 
-            paddings = tf.constant([[0, 0], [2, 2], [0, 0]])
-            self.seq_tokens_embeddings = tf.pad(self.seq_tokens_embeddings, paddings, "CONSTANT")
-
-            self.seq_tokens_embeddings = tf.map_fn(self.token_sums, tf.range(0, batch_seq_len, 1), parallel_iterations=1, dtype=(tf.float32)) # max seq len x batch size x emb dim
-
-            # perm dims
-            self.seq_tokens_embeddings = tf.transpose(self.seq_tokens_embeddings, perm=[1, 0, 2]) # batch size x max seq len x emb dim
-
             return pool_sequence_embedding(self.get_hyper('nbow_pool_mode').lower(),
-                                           sequence_token_embeddings=self.seq_tokens_embeddings,
+                                           sequence_token_embeddings=seq_tokens_embeddings,
                                            sequence_lengths=seq_token_lengths,
                                            sequence_token_masks=seq_token_mask,
                                            is_train=is_train)
-
-    def token_sums(self, t):
-        x = self.seq_tokens_embeddings
-
-        context = tf.concat( [ x[:, t:t+2, :], x[:, t+3:t+5, :] ], dim=1) # batch size x 4 x emb dim
-
-        return tf.reduce_sum(context, 1) # batch size x emb dim
